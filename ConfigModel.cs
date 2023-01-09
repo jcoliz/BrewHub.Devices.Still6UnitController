db18d97e09db5d50761a92c518781ddad75706fe
James Coliz <jcoliz@outlook.com>
2023-01-09 11:10:34 -0800

    feat: Add toml config model

---
+++ ConfigModel.cs
@@ -0,0 +1,45 @@
+using Tomlyn.Model;
+
+namespace BrewHub.Controller;
+
+public class ConfigModel : ITomlMetadataProvider
+{
+    public Provisioning? Provisioning { get; set; }
+
+    // storage for comments and whitespace
+    TomlPropertiesMetadata? ITomlMetadataProvider.PropertiesMetadata { get; set; }
+}
+
+public class Provisioning : ITomlMetadataProvider
+{
+    public string? Source { get; set; }
+
+    public string? GlobalEndpoint { get; set; }
+
+    public string? IdScope { get; set; }
+
+    public Attestation? Attestation { get; set; }
+
+    // storage for comments and whitespace
+    TomlPropertiesMetadata? ITomlMetadataProvider.PropertiesMetadata { get; set; }
+}
+
+public class Attestation : ITomlMetadataProvider
+{
+    public string? Method { get; set; }
+
+    public string? RegistrationId { get; set; }
+
+    public SymmetricKey? SymmetricKey { get; set; }
+
+    // storage for comments and whitespace
+    TomlPropertiesMetadata? ITomlMetadataProvider.PropertiesMetadata { get; set; }
+}
+
+public class SymmetricKey : ITomlMetadataProvider
+{
+    public string? Value { get; set; }
+
+    // storage for comments and whitespace
+    TomlPropertiesMetadata? ITomlMetadataProvider.PropertiesMetadata { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BrewHub.Controller;
+using Tomlyn;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("BrewHub Controller");
+
+var model = new ConfigModel();
+model.Provisioning = new Provisioning() { Source = "Source", GlobalEndpoint = "GlobalEndpoint", IdScope = "IdScope" };
+model.Provisioning.Attestation = new() { Method = "Method", RegistrationId = "RegistrationId" };
+model.Provisioning.Attestation.SymmetricKey = new() { Value = "1234" };
+
+var tomlOut = Toml.FromModel(model);
+Console.WriteLine(tomlOut);
