b460eb8a6b42e0106d05874d0150d737865bf1f9
James Coliz <jcoliz@outlook.com>
2023-01-09 15:02:51 -0800

    feat(controller): Moved to background service. Added logging

---
+++ ControllerBackgroundService.cs
@@ -0,0 +1,131 @@
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using Tomlyn;
+
+namespace BrewHub.Controller;
+
+public sealed class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    private DeviceClient? iotClient;
+    private SecurityProviderSymmetricKey? security;
+    private DeviceRegistrationResult? result;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await ProvisionDevice();
+        await OpenConnection();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await SendTelemetry();
+            await Task.Delay(30_000, stoppingToken);
+
+        }
+
+        if (iotClient is not null)
+            await iotClient.CloseAsync();
+
+        _logger.LogInformation("Finished.");
+    }
+
+    private async Task ProvisionDevice()
+    {
+        try
+        {
+            using var reader = File.OpenText("config.toml");
+            var toml = reader.ReadToEnd();
+
+            var config = Toml.ToModel<ConfigModel>(toml);
+
+#if false
+            var options = new System.Text.Json.JsonSerializerOptions() { WriteIndented = true };
+            var json = System.Text.Json.JsonSerializer.Serialize(config,options);
+            Console.WriteLine("Config:");
+            Console.WriteLine(json);
+#endif
+
+            _logger.LogInformation($"Initializing the device provisioning client...");
+
+            // For group enrollments, the second parameter must be the derived device key.
+            // See the ComputeDerivedSymmetricKeySample for how to generate the derived key.
+            // The secondary key could be included, but was left out for the simplicity of this sample.
+            security = new SecurityProviderSymmetricKey(
+                config!.Provisioning!.Attestation!.RegistrationId,
+                config!.Provisioning!.Attestation!.SymmetricKey!.Value,
+                null);
+
+            using ProvisioningTransportHandler transportHandler = new ProvisioningTransportHandlerHttp();
+
+            var endpoint = new Uri(config!.Provisioning!.GlobalEndpoint!);
+            var provClient = ProvisioningDeviceClient.Create(
+                endpoint.Host,
+                config.Provisioning.IdScope,
+                security,
+                transportHandler);
+
+            _logger.LogInformation($"Initialized for registration Id {security.GetRegistrationID()}.");
+
+            _logger.LogInformation("Registering with the device provisioning service...");
+            result = await provClient.RegisterAsync();
+
+            _logger.LogInformation($"Registration status: {result.Status}.");
+            if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+            {
+                _logger.LogError($"Registration status did not assign a hub, so exiting this sample.");
+                return;
+            }
+
+            _logger.LogInformation($"Device {result.DeviceId} registered to {result.AssignedHub}.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("ERROR: {message}", ex.Message);
+        }
+    }
+
+    private async Task OpenConnection()
+    {
+        try
+        {
+            // NOTE we can now store the device registration result to storage, and use it next time
+            // to not have to run the above registration flow again
+
+            _logger.LogInformation("Creating symmetric key authentication for IoT Hub...");
+            IAuthenticationMethod auth = new DeviceAuthenticationWithRegistrySymmetricKey(
+                result!.DeviceId,
+                security!.GetPrimaryKey());
+
+            _logger.LogInformation($"Testing the provisioned device with IoT Hub...");
+            iotClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Mqtt);
+
+    #if false        
+            // Attach a callback for updates to the module twin's desired properties.
+            await _moduleClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertiesUpdate, null);
+
+            // Register callback for health check command
+            await _moduleClient.SetMethodHandlerAsync(HealthCheckCommand, HealthCheckAsync, null);
+    #endif
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("ERROR: {message}", ex.Message);
+        }
+    }
+
+    private async Task SendTelemetry()
+    {
+        var text = "TestMessage";
+        using var message = new Message(Encoding.UTF8.GetBytes(text));
+        await iotClient!.SendEventAsync(message);
+        _logger.LogInformation("Sent message: {text}", text);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,79 +1,13 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using BrewHub.Controller;
-using Microsoft.Azure.Devices.Client;
-using Microsoft.Azure.Devices.Provisioning.Client;
-using Microsoft.Azure.Devices.Provisioning.Client.Transport;
-using Microsoft.Azure.Devices.Shared;
-using Tomlyn;
 
-try
-{
-    // See https://aka.ms/new-console-template for more information
-    Console.WriteLine("BrewHub Controller");
-
-    using var reader = File.OpenText("config.toml");
-    var toml = reader.ReadToEnd();
-
-    var config = Toml.ToModel<ConfigModel>(toml);
-
-    var options = new System.Text.Json.JsonSerializerOptions() { WriteIndented = true };
-    var json = System.Text.Json.JsonSerializer.Serialize(config,options);
-
-    Console.WriteLine("Config:");
-    Console.WriteLine(json);
-
-    Console.WriteLine($"Initializing the device provisioning client...");
-
-    // For group enrollments, the second parameter must be the derived device key.
-    // See the ComputeDerivedSymmetricKeySample for how to generate the derived key.
-    // The secondary key could be included, but was left out for the simplicity of this sample.
-    using var security = new SecurityProviderSymmetricKey(
-        config!.Provisioning!.Attestation!.RegistrationId,
-        config!.Provisioning!.Attestation!.SymmetricKey!.Value,
-        null);
-
-    using ProvisioningTransportHandler transportHandler = new ProvisioningTransportHandlerHttp();
-
-    var endpoint = new Uri(config!.Provisioning!.GlobalEndpoint!);
-    var provClient = ProvisioningDeviceClient.Create(
-        endpoint.Host,
-        config.Provisioning.IdScope,
-        security,
-        transportHandler);
-
-    Console.WriteLine($"Initialized for registration Id {security.GetRegistrationID()}.");
-
-    Console.WriteLine("Registering with the device provisioning service...");
-    DeviceRegistrationResult result = await provClient.RegisterAsync();
-
-    Console.WriteLine($"Registration status: {result.Status}.");
-    if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
     {
-        Console.WriteLine($"Registration status did not assign a hub, so exiting this sample.");
-        return;
-    }
+        services.AddHostedService<Worker>();
+    })
+    .Build();
 
-    Console.WriteLine($"Device {result.DeviceId} registered to {result.AssignedHub}.");
+await host.RunAsync();
 
-    // NOTE we can now store the device registration result to storage, and use it next time
-    // to not have to run the above registration flow again
-
-    Console.WriteLine("Creating symmetric key authentication for IoT Hub...");
-    IAuthenticationMethod auth = new DeviceAuthenticationWithRegistrySymmetricKey(
-        result.DeviceId,
-        security.GetPrimaryKey());
-
-    Console.WriteLine($"Testing the provisioned device with IoT Hub...");
-    using var iotClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Mqtt);
-
-    Console.WriteLine("Sending a telemetry message...");
-    using var message = new Message(Encoding.UTF8.GetBytes("TestMessage"));
-    await iotClient.SendEventAsync(message);
-
-    await iotClient.CloseAsync();
-    Console.WriteLine("Finished.");
-}
-catch (Exception ex)
-{
-    Console.WriteLine("ERROR: {0}", ex.Message);
-}
